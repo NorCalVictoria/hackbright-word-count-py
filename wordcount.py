0e652d7311cc87a0cc997561da5277b2b783ec7a
Hackbright Student <no-reply@hackbrightacademy.com>
2018-09-10 13:06:04 -0700

    Fixed wordcount.py

--- wordcount.py
+++ wordcount.py
@@ -8,14 +8,21 @@ def word_counter(tst):
     for line in tst:
         line.rstrip()
         new_line = line.split()
-        
+
         for word in new_line:
-
             word_dict[word] = word_dict.get(word,0) + 1
-        
-    return word_dict
+
+    
+    for word in word_dict.items():
+        lst = (f'{word[0]} : {word[1]}')
+        print(lst)
+    
+    return word_dict        
+      
 
 
-    #print(each_word)
+
 
 print(word_counter(tst))
+
+# How to remove punctuation to avoid duplicate words ?
